240d00e23095e2d0ac7d393930b692624d2277ea
gustavo-exe <gus.elim2@gmail.com>
2021-08-24 21:41:21 -0600

    Haciendo la peticion a la API directamente en el componente.

--- src/components/organisms/Main.jsx
+++ src/components/organisms/Main.jsx
@@ -1,13 +1,63 @@
-import React from 'react';
-import instance from "../../api/api";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import "../../styles/Main.css";
+
+let subscriptionKey = process.env.REACT_APP_API_SUBSCRIPTION_KEY;
+let endpoint = process.env.REACT_APP_API_END_POINT + '/face/v1.0/detect';  
 
 const Main = () => {
-    console.log(instance);
-    console.log(process.env.REACT_API_END_POINT);
-    return(
+
+    const [data, setData] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
+
+    const handleOnChange = event => {
+        setImageUrl(event.target.value)
+    }
+
+    const handleClickImage = async event => {
+        event.preventDefault();
+        axios({
+            method: 'post',
+            url: endpoint,
+            params: {
+                detectionModel: 'detection_03',
+                returnFaceId: true
+            },
+            data: {
+                url: imageUrl
+            },
+            headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey }
+        }).then(function (response) {
+            setData(response.data)
+            console.log(response.data)
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    return (
         <main>
             <section>
-                Contenido principal
+                <input
+                    type="text"
+                    placeholder="Coloca el link de la imagen"
+                    onChange={handleOnChange}
+                    value={imageUrl} style={{ width: "450px" }}
+                />
+
+                <button
+                    className="buttonFile"
+                    onClick={handleClickImage}
+                >
+                    Analizar
+                </button>
+            </section>
+
+            <section>
+                <img
+                    src={imageUrl}
+                    alt="Aqui aparecera la imagen"
+                />
             </section>
         </main>
     )
